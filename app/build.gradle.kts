aa7925f4354cd5378f2fa333a11694fda5d03eb2
javix142-gif <javix142@gmail.com>
2026-04-07 23:59:44 -0400

    Update composeOptions in app/build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,6 +32,9 @@ android {
     }
     kotlinOptions { jvmTarget = "17" }
     buildFeatures { compose = true }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "2.0.0"
+    }
 }
 
 dependencies {
